3f1aa75c56b0e7c5be768338720c486ece7c9cf3
KnzwaT <64577036+KanazawaT@users.noreply.github.com>
2025-04-26 01:58:33 +0900

    カモ守りドッヂチーム分け完成

--- kamomori.js
+++ kamomori.js
@@ -6,6 +6,13 @@ function handleClick() {
         .map(line => line.trim())
         .filter(line => line.length > 0);
 
+    const jobText = document.getElementById("jobList").value;
+
+    const jobs = jobText
+        .split("\n")
+        .map(line => line.trim())
+        .filter(line => line.length > 0);
+
     const shuffled = names.sort(() => Math.random() - 0.5);
 
     let team1 = [];
@@ -20,6 +27,13 @@ function handleClick() {
     team2 = shuffled.slice(maxPerTeam, assigned);
     extra = shuffled.slice(assigned);
 
+    for(i = 0;i < maxPerTeam;i++){
+        j = i + maxPerTeam;
+        team1[i] += "(" + jobs[i] + ")";
+        team2[i] += "(" + jobs[j] + ")";
+    }
+
+
     // 出力
     document.getElementById("team1").value = team1.join("\n");
     document.getElementById("team2").value = team2.join("\n");
@@ -29,11 +43,14 @@ function handleClick() {
     console.log("東:", team2);
     console.log("観戦:", extra);
     // チーム分け後、以下を末尾に追加するなど
-    const resultStr =
+    resultStr = /*"";
+    resultStr += "西：";
+    for(i = 0;i < 3;i++){
+        resultStr += team1[i] + "(" + jobs[i] + ")、";
+    }*/
     `西：${team1.join("、")}\n` +
     `東：${team2.join("、")}\n` +
     (extra.length > 0 ? `観戦：${extra.join("、")}` : "");
-
     document.getElementById("resultText").value = resultStr;
 }
 
